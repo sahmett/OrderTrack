1fbe570d255096aed1e94e54c016dd79f208f237
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2024-03-13 17:38:34 +0300

    added: domain entities classes

---
+++ src/Core/OrderTrack.Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrack.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ src/Core/OrderTrack.Domain/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTrack.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid CustomerId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+        public Address Address { get; set; }
+        public Product Product { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
+
